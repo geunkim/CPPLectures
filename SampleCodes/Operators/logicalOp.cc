d1a6a6165ed8ca50e07d74816c23d7ed94dc8b5f
Geun-Hyung Kim <geunkim@deu.ac.kr>
2020-04-15 22:16:59 +0900

    Update logicalOp.cc

--- SampleCodes/Operators/logicalOp.cc
+++ SampleCodes/Operators/logicalOp.cc
@@ -13,15 +13,15 @@ int main(int argc, char const *argv[])
 	bool testBoolA = true, testBoolB = false;
 	int *ptr = NULL;
 
-	cout << (testVarA == 10 || testBoolB) << endl;   	 // true
-	cout << ((testBoolA) && (testVarA != 10)) << endl;   // false
-	cout << (testBoolA && !testBoolB) << endl;		     // true
-	cout << (testVarA >= 0 && testVarA <= 20) << endl;   // true
-	cout << (testVarA < 0 || testVarA > 20) << endl;     // false
+	cout << (testVarA == 10 || testBoolB) << endl;   	// true
+	cout << ((testBoolA) && (testVarA != 10)) << endl;  	// false
+	cout << (testBoolA && !testBoolB) << endl;		// true
+	cout << (testVarA >= 0 && testVarA <= 20) << endl;   	// true
+	cout << (testVarA < 0 || testVarA > 20) << endl;     	// false
 	//  단락 논리
-	cout << (ptr == NULL || *ptr == 10) << endl;         // true
+	cout << (ptr == NULL || *ptr == 10) << endl;         	// true
 	
-	// cout << (*ptr == 10) << endl;				     // Segmentation fault
+	// cout << (*ptr == 10) << endl;			// Segmentation fault
 
 	return 0;
 }
