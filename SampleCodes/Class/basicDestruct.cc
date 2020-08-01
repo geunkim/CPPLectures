789a214f03ba94347a37922b8827a6c41cb14b96
Geun-Hyung Kim <geunkim@deu.ac.kr>
2020-08-01 23:30:17 +0900

    Create basicDestruct.cc

---
+++ SampleCodes/Class/basicDestruct.cc
@@ -0,0 +1,37 @@
+/*
+	File name: basicDestruct.cc
+	Author: Geun-Hyung Kim
+*/
+
+/*
+	File name: defaultConstruct.cc
+	Author: Geun-Hyung Kim
+*/
+#include <iostream>
+using namespace std;
+
+class Person{
+private:
+	string name;
+	int age;
+public:
+	~Person();
+	void setName(string n) { name = n;}
+	void setAge(int a) { age = a;}
+	void show() {
+		cout << name << ", " << age << endl;
+	}
+};
+
+Person::~Person() {
+	cout << "소멸자 호출" << endl;
+}
+
+int main(int argc, char const *argv[])
+{
+	Person s;  // 컴파일러가 자동 생성한 기본 생성자 호출 
+	s.setName("Chul-soo Lee");
+	s.setAge(20);
+	s.show();
+	return 0;
+}
