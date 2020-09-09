cb1d409c537a0d7f1f68dbe27fd2a876cb9955d5
Geun-Hyung Kim <geunkim@deu.ac.kr>
2020-09-09 10:40:08 +0900

    Create dateClass.cc

---
+++ SampleCodes/Class/dateClass.cc
@@ -0,0 +1,26 @@
+*
+	File name: dateClass.cc
+	Author: Geun-Hyung Kim
+*/
+
+class Date{
+	int year;
+	int month;
+	int day;
+	Date()
+	Date(int y, int m, int d);
+	~Date();
+	void setYear(int y);
+	void setMonth(int m);
+	void setDay(int d);
+	int getYear();
+	int getMonth();
+	int getDay();
+	void printDate();
+};
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	return 0;
+}
