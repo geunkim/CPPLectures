ce4981c32db2c59daab9d00384f27ba5eceebc5b
Geun-Hyung Kim <geunkim@deu.ac.kr>
2020-09-09 10:43:31 +0900

    Create Person.cc

---
+++ SampleCodes/Class/Person.cc
@@ -0,0 +1,18 @@
+*
+	File name: Person.cc
+	Author: Geun-Hyung Kim
+*/
+
+class Person {
+	int age;
+	Person();
+	void setAge(int age);
+	int getAge();
+	/* 멤버 변수와 멤버 함수를 추가하시오 */
+};
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	return 0;
+}
