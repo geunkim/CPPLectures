3b41c79b8b8662e1fdffe39f22416dfc11cd1bc2
Geun-Hyung Kim <geunkim@deu.ac.kr>
2020-09-09 10:35:36 +0900

    Create simpleClass.cc

---
+++ SampleCodes/Class/simpleClass.cc
@@ -0,0 +1,24 @@
+/*
+	File name: simpleClass.cc
+	Author: Geun-Hyung Kim
+*/
+
+class Point{
+	double x;
+	double y;
+	double getX();
+	double getY();
+	Point();
+	Point(int x, int y);
+	~Point();
+	void setX(int x);
+	void setY(int y);
+	void printPoint();
+	/* 멤버 함수를 추가하시요 */
+};
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	return 0;
+}
