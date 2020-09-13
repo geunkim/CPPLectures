b855dce4d97824c23b8ceaa556fcb2c43c81e9f5
Geun-Hyung Kim <geunkim@deu.ac.kr>
2020-09-14 03:39:07 +0900

    Update carClass.cc

--- SampleCodes/Class/carClass.cc
+++ SampleCodes/Class/carClass.cc
@@ -12,13 +12,15 @@ class Car
 	double gas;
 public:
 	Car(int n = 2271, double g = 555.9);
-	int getNum(){return num;} // 인라인 함수
-	double getGas() {return gas;} // 인라인 함수
+	int getNum(){return num;} // 인라인 멤버함수
+	double getGas() {return gas;} // 인라인 멤버함수
 	void show();
 };
 
+// 생성자
 Car::Car(int n, double g) : num(n), gas(g) {}
 
+// 일반 멤버함수 
 void Car::show() {
 	cout << "Number: " << num << endl;
 	cout << "Gas: " << gas << endl;
@@ -30,3 +32,5 @@ int main(int argc, char const *argv[])
 	h.show();
 	return 0;
 }
+
+
