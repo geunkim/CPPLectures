7fa1be9a36a9cf681a568ed83b63466dbe2a8b1c
Peter Kim <geunkim@deu.ac.kr>
2020-12-14 11:03:43 +0900

    Create prob_1.cpp

---
+++ SampleCodes/FinalExam/prob_1.cpp
@@ -0,0 +1,21 @@
+/*
+	File name: prob_1.cpp
+	Author: Geun-Hyung kim
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	cout << "이름을 입력하시요: ";
+	string name;  // 변수 이름 변경한 후 처리하시오.
+
+	cin >> name;
+
+	cout << name;
+
+	return 0;
+}
