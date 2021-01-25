42ed12c8ade27fc82e5c350a437f9bbe07a80042
Peter Kim <geunkim@deu.ac.kr>
2021-01-25 18:25:21 +0900

    Create stringIn.cc
    getline() 메소드 활용 프로그램

---
+++ SampleCodes/InOut/stringIn.cc
@@ -0,0 +1,18 @@
+/*
+	File: stringIn.cc
+
+	Author: Geun-Hyung Kim
+*/
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	char arr[20];
+	cout << "문자열을 입력하시오:";
+	cin >> arr;
+	cout << arr << endl;
+
+	return 0;
+}
