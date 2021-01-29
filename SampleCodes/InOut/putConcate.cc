28e9d3f02c246f0c656ca603249dafda2f37ceb1
Peter Kim <geunkim@deu.ac.kr>
2021-01-29 12:51:33 +0900

    Create putConcate.cc

---
+++ SampleCodes/InOut/putConcate.cc
@@ -0,0 +1,14 @@
+/* 
+	File: putConcate.cc
+
+	Author: Geun-Hyung Kim
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	cout.put('A').put('B').put('C').put('D').put('\n');
+	return 0;
+}
