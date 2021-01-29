ca407623e5ae47ee10df8571944ddf0c54f1b3f4
Peter Kim <geunkim@deu.ac.kr>
2021-01-29 12:54:14 +0900

    Create prob_4.cpp

---
+++ SampleCodes/FinalExam/prob_4.cpp
@@ -0,0 +1,23 @@
+/*
+	File name: prob_2.cpp
+	Author: Geun-Hyung kim
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	vector<int> data;
+	int x;
+	
+	
+	do {
+		cin >> x;
+
+	}while(x > 0);
+
+	return 0;
+}
