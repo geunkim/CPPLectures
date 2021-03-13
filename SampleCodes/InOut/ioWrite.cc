401dd50d1f0d5ed2894dbefde440a4d1eecf9179
Peter Kim <geunkim@deu.ac.kr>
2021-03-14 01:37:49 +0900

    Update ioWrite.cc

--- SampleCodes/InOut/ioWrite.cc
+++ SampleCodes/InOut/ioWrite.cc
@@ -1,7 +1,9 @@
 /*
 	File: ioWrite.cc
-
 	Author: Geun-Hyung Kim
+	Description:
+	"Hello World!" 문자열을 str 배열에 저장하고 배열에 저장된 데이터를 write 함수를 
+	이용하여 콘솔화면에 출력하는 프로그램 
 */
 
 #include <iostream>
@@ -9,9 +11,10 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-	char str[] = "Hello world!";
+	char str[] = "Hello World!";  // "Hello World!" 문자열을 배열에 저장
 
-	cout.write(str, strlen(str));
-	cout.put('\n');
+	cout.write(str, strlen(str)); // 배열에 저장한 데이터를 cout.write 함수로 콘솔에 출력 
+	cout.put('\n');               // 콘솔에 줄바꿈 문자를 출력 
+	
 	return 0;
 }
