ffb18b782597e558932e5f930fa3170c33f488bf
Peter Kim <geunkim@deu.ac.kr>
2021-03-29 21:26:19 +0900

    Create multiCompare.cc

---
+++ SampleCodes/Control/multiCompare.cc
@@ -0,0 +1,25 @@
+/*
+
+	File Name: multiCompare.cc
+	Author: Geun-Hyung Kim
+
+	Description: 두 개의 정수의 값을 비교하여 대소를 표시한다. 
+	Date: 2021. 3. 29
+	Version: 0. 1. 0
+*/
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int x = 20;
+	int y = 20;
+
+	if (x > y)
+		cout << "x(" << x << ")는 " << "y(" << y << ")보다 크다." << endl;
+	else if (y > x)
+		cout << "y(" <<y << ")는 " << "x(" <<x << ")보다 크다." << endl;
+	else 
+		cout << "x(" << x << ")는 " << "y(" << y << ")와 같다." << endl;
+	return 0;
+}
