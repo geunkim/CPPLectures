15fc509e17cf29c5461b9da77a2649d344bc4f20
Peter Kim <geunkim@deu.ac.kr>
2021-04-06 15:00:17 +0900

    Create charArray.cc

---
+++ SampleCodes/Array/charArray.cc
@@ -0,0 +1,23 @@
+/*
+
+	File Name: charArray.cc
+	Author: Geun-Hyung Kim
+
+	Description: 
+	문자열을 저장하기 위한 문자 배열 사용 
+	Date: 2021. 4. 2
+	Version: 0. 1. 0
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	char str[] = "Hello World!";
+	cout << str << endl;
+
+
+	return 0;
+}
