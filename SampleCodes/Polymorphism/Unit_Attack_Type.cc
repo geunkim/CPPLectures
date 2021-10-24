6ff81149194bb159b8ef5662aadc6ab01f3e0b98
SeungminYeom <81202999+SeungminYeom@users.noreply.github.com>
2021-10-24 23:59:14 +0900

    .

--- SampleCodes/Polymorphism/Unit_Attack_Type.cc
+++ SampleCodes/Polymorphism/Unit_Attack_Type.cc
@@ -8,14 +8,14 @@ public:
 	Unit_Attack() { cout << "Unit_Attack_Type" << endl; }
 	virtual ~Unit_Attack() { cout << "Type_Delete" << endl; }
 
-	virtual void attackType() = 0; // ĳ������ ����Ÿ���� �����Ͻʽÿ�
+	virtual void attackType() = 0; // 캐릭터의 공격타입을 지정하십시오
 };
 
 class Barbarian : public Unit_Attack {
 public:
 	Barbarian() { cout << "[Barbarian]" << endl; }
 	~Barbarian() { cout << "class_delete" << endl; }
-	void attackType() { cout << "�ٰŸ�\n" << endl; }
+	void attackType() { cout << "근거리\n" << endl; }
 };
 
 class Archer : public Unit_Attack {
@@ -23,7 +23,7 @@ public:
 	Archer() { cout << "[Archer]" << endl; }
 	~Archer() { cout << "class_delete" << endl; }
 
-	void attackType() { cout << "���Ÿ�\n" << endl; }
+	void attackType() { cout << "원거리\n" << endl; }
 };
 
 class Wizard : public Unit_Attack {
@@ -31,7 +31,7 @@ public:
 	Wizard() { cout << "[Wizard]" << endl; }
 	~Wizard() { cout << "class_delete" << endl; }
 
-	void attackType() { cout << "����\n" << endl; }
+	void attackType() { cout << "마법\n" << endl; }
 };
 
 int main() {
