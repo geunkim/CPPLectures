e92e33a39a536671dc23374b6d3d7d183a8c70ea
Geun-Hyung Kim <geunkim@deu.ac.kr>
2021-12-27 17:40:42 +0900

    Create problem2.cc

---
+++ SampleCodes/CppBasic/problem2.cc
@@ -0,0 +1,19 @@
+/*
+	File: problem2.cc
+	Author: Geun-Hyung Kim
+	Problem: 
+	두 개의 정수를 커보드를 부터 입력을 받아 변수 first, second에 저장한 후 
+	화면에 출력하도록 하는 코드 작성
+
+*/
+
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+	int first, second;
+
+
+	cout << first << ", " << second << endl;
+	return 0;
+}
