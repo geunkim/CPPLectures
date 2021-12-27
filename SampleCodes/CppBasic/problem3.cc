92edef690855d7ade912784ed8bfe4d89bbd3078
Geun-Hyung Kim <geunkim@deu.ac.kr>
2021-12-27 17:40:38 +0900

    Create problem3.cc

---
+++ SampleCodes/CppBasic/problem3.cc
@@ -0,0 +1,22 @@
+/*
+	File: problem3.cc
+	Author: Geun-Hyung Kim
+	Problem: 
+	키보드에서 Hello world! 를 입력한 것을 변수 buf에 저장하고 
+	buf에 저장된 내용을 화면에 출력하는 프로그램 완성 
+
+*/
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	char buf[255];
+	
+
+
+	cout << buf << endl;
+
+	return 0;
+}
