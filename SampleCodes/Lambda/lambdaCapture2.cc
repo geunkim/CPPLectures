e2059b1be08ea05938991035fc1f01d0cab05fc2
Geun-Hyung Kim <geunkim@deu.ac.kr>
2023-11-20 08:34:06 +0900

    Create lambdaCapture2.cc

---
+++ SampleCodes/Lambda/lambdaCapture2.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int total = 10;
+    int factor = 20;
+
+    auto lambdatest = [=] { total += 30; factor += 10; cout << factor << endl; };
+
+    lambdatest();
+
+    cout << "total: " << total << endl;
+    cout << "factor: " << factor << endl;
+
+    return 0;
+}
